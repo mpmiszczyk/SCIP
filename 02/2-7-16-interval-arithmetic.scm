6e0ca31a8b5a91e24540d61fea86ff39f498e98b
Marcin Miszczyk <mpmiszczyk@gmail.com>
2016-11-15 19:32:39 +0100

    2.7 to 2.10

--- 02/2-7-16-interval-arithmetic.scm
+++ 02/2-7-16-interval-arithmetic.scm
@@ -7,10 +7,27 @@
 ;;;; range of the result.
 
 
+(define (make-interval lower upper)
+  (cons lower upper))
+
+(define lower-bound car)
+
+(define upper-bound cdr)
+
 (define (add-interval x y)
   (make-interval (+ (lower-bound x) (lower-bound y))
                  (+ (upper-bound x) (upper-bound y))))
 
+(define (sub-interval x y)
+  (make-inteval (- (lower-bound x) (upper-bound y))
+                (- (upper-bound x) (lower-bound y))))
+
+;; (= ((- 2 2) . (- 3 1))
+;;    (sub-interval (2 . 3)
+;;                  (1 . 2)))
+
+(define (width-interval x)
+  (- (upper-bound x) (lower-bound x)))
 
 (define (mul-interval x y)
   (let ((p1 (* (lower-bound x) (lower-bound y)))
@@ -24,6 +41,26 @@
   (mul-interval x
                 (make-interval (/ 1 (upper-bound y))
                                (/ 1 (lower-bound y)))))
+
+;; 2.10 will raise
+(define (div-interval x y)
+  (if (not (spans-over-zero? y))
+      (mul-interval x
+                    (make-interval (/ 1 (upper-bound y))
+                                   (/ 1 (lower-bound y))))
+      (error "division over zero not allowed" y)))
+
+
+;; 2.9 in some way it is a funciton, but not a monotonic one
+
+(define-test (same-width-from-different-widths)
+  (let ((a (make-interval 0 2))
+        (b (make-interval 2 2))
+        (c (make-interval 0 2)))
+    (check (not (= (width-interval b)
+                   (width-interval c))))
+    (check (= (width-interval (mul-interval a b))
+              (width-interval (mul-interval a c))))))
 
 
 (run-registered-tests)
